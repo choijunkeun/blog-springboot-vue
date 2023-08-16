cbba31ac72f78b651a8a015d7e64c2568ea45606
jan <bonawillis@naver.com>
2023-08-17 00:59:38 +0900

    init commit

---
+++ front/vue.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  outputDir: '../src/main/resources/static', // Build Directory 설정
+      devServer: {
+            proxy: 'http://localhost'     // Spring boot Server 설정
+      }
+})
